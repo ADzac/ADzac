0228708c91b6b0ca70d71a12777bf5c9c31ff738
ADzac <danielzack786@gmail.com>
2022-03-15 19:16:57 +0100

    Create connect.py

---
+++ connect.py
@@ -0,0 +1,24 @@
+import socket
+from socket import socket
+
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+
+print(local_ip)
+HOST = local_ip  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(2048)
+            if not data:
+                break
+            conn.sendall(data)
+            conn.send("I'm Server".encode())
+            print("Client : ", data.decode())
+    conn.close()
