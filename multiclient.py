5772c0ca4d59252238ff7122c327140efd06a421
ADzac <danielzack786@gmail.com>
2022-03-16 16:55:57 +0100

    Create multiclient.py

---
+++ multiclient.py
@@ -0,0 +1,16 @@
+import socket
+ClientMultiSocket = socket.socket()
+host = '134.59.139.201'
+port = 65432
+print('Waiting for connection response')
+try:
+    ClientMultiSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
+res = ClientMultiSocket.recv(1024)
+while True:
+    Input = input('Hey there: ')
+    ClientMultiSocket.send(str.encode(Input))
+    res = ClientMultiSocket.recv(1024)
+    print(res.decode('utf-8'))
+ClientMultiSocket.close()
