b2db4730629c21a0d59be337f1340b0a6c145a0a
ADzac <danielzack786@gmail.com>
2022-03-16 07:18:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ def restart(args):
     if args == 1 and os_name == 'Linux':
         os.system('/sbin/shutdown -r +10')
         print("Restarting")
+    if args == 1 and os_name == 'Windows':
+        os.system('shutdown /r /m \\[MY REMOTE PC] /t 60 /c "Please finish your work"')
+        print("Restarting")
     if args == 2 and os_name == 'Linux':
         os.system('/sbin/shutdown +10')
+        print("Shutting Down")
+    if args == 2 and os_name == 'Windows':
+        os.system('shutdown /s /m \\[MY REMOTE PC] /t 60 /c "Please finish your work"')
         print("Shutting Down")
     else:
         exit()
@@ -25,3 +31,10 @@ btnShut = tkinter.Button(gui, text="Shutdown", command=lambda: restart(2))
 btnRes.pack()
 btnShut.pack()
 gui.mainloop()
+
+file = open("webpage.html", "r")
+
+
+
+
+file.close()
