2c34f92d1239a3e8adfb23059c250efe48b8c925
ADzac <danielzack786@gmail.com>
2022-03-26 14:24:55 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,26 +1,25 @@
+from multiprocessing import connection
 import socket
 import os
 from _thread import *
-
+import tkinter
 
 ServerSideSocket = socket.socket()
 hostname = socket.gethostname()
-host = socket.gethostbyname(hostname) #host take the value of own IP address
+host = socket.gethostbyname(hostname)
 port = 65432
 ThreadCount = 0
 
-local_ip = socket.gethostbyname(hostname)
-print(local_ip) #show server's IP address
+print(host)
 
 try:
-	ServerSideSocket.bind((host, port))
+    ServerSideSocket.bind((host, port))
 except socket.error as e:
-	print(str(e))
-print('Socket is listening..')
+    print(str(e))
+    print('Socket is listening..')
 
 ServerSideSocket.listen(5)
 temp_list = []
-
 
 always=open("web.html","r")
 alwayscontent=always.readlines()
@@ -28,30 +27,49 @@ always.close()
 renew=open("renew.html","w")
 
 for x in alwayscontent:
-	renew.write(x) #copy web.html into renew.html
-  
+    renew.write(x)
 renew.close()
 
+def window():
+    gui = tkinter.Tk()
+    gui.geometry("300x300")
+    btnRes = tkinter.Button(gui, text="Restart", command=lambda: restart(1))
+    btnShut = tkinter.Button(gui, text="Shutdown", command=lambda: restart(2))
+    btnRes.pack()
+    btnShut.pack()
+    gui.mainloop()
+
+def restart(args):
+    if args == 1:
+        command = 'shutdown'
+        Client.send(command.encode())
+    if args == 2 :
+        command = 'restart'
+        Client.send(command.encode())
+    else:
+        exit()
 
 def multi_threaded_client(connection):
-	connection.send(str.encode('Server is working:'))
-	while True:
-		data = connection.recv(1024) #receive message from client
-		if not data:
-			break
-		l=data.decode()
-		renew=open("renew.html","a")
-		renew.write(l) #write the message(table) from client and put in renew.html
-		renew.close()
-	connection.close()
+    connection.send(('Server is working!!').encode())
+    while True:
+        data = connection.recv(1024)
+        l=data.decode()
+        renew=open("web.html","a")
+        f = open('test.html','w')
+        f.write(l)
+        f.close()
+        renew.write(l)
+        renew.close()
+        if not data:
+            break
+    connection.close()
 
-	
 while True:
-	Client, address = ServerSideSocket.accept()
-	print('Connected to: ' + address[0] + ':' + str(address[1]))
-	start_new_thread(multi_threaded_client, (Client, ))
-	ThreadCount += 1
-	print('Thread Number: ' + str(ThreadCount))
-	
-	
+    Client, address = ServerSideSocket.accept()
+    print('Connected to: ' + address[0] + ':' + str(address[1]))
+    start_new_thread(multi_threaded_client, (Client, ))
+    ThreadCount += 1
+    print('Thread Number: ' + str(ThreadCount))
+    #while connected == True:
+    #window()
 ServerSideSocket.close()
